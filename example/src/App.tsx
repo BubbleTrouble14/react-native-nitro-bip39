e8f5f5f2ca07740e636c04726a05e7716a12e7c8
Ronald <ronald.goedeke@outlook.com>
2024-10-15 19:41:06 -0300

    small update

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,16 +1,8 @@
 import * as React from 'react'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
-import { bip39 } from 'react-native-nitro-bip39'
 import { HybridObjectTestsScreen } from './screens/HybridObjectTestsScreen'
 
 export default function App() {
-  React.useEffect(() => {
-    const seed = bip39.mnemonicToSeed(
-      'zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo vote',
-      'TREZOR'
-    )
-    console.log(Buffer.from(seed).toString('hex'))
-  }, [])
   return (
     <SafeAreaProvider>
       <HybridObjectTestsScreen />
